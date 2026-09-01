24fcca02b77b60405898f372879f22c81b5af91a
AbolfazlMohammadi <abolfazl.mohammadi.85.2006@gmail.com>
2026-09-01 23:21:07 +0330

    last nigth commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import GlobalStyle from "./styles/GlobalStyle.js";
 import Button from "./ui/Button.jsx";
 import Input from "./ui/Input.jsx";
 import Heading from "./ui/Heading.jsx";
+import Row from "./ui/Row.jsx";
 
 const test = css`
     text-align: center;
 `;
 const StyledApp = styled.div`
-    background-color: orangered;
     padding: 20px;
     ${test}
 `;
@@ -18,16 +18,27 @@ function App() {
         <>
             <GlobalStyle/>
             <StyledApp>
-                <Heading as="h1">The wild oasis</Heading>
-                <Heading as="h2">check in and out</Heading>
-                <Button>check in</Button>
-                <Button>check out</Button>
-                <Heading as="h3">Form</Heading>
-                <Input type="number" placeholder="Number of guests"/>
+                <Row>
+                    <Row type="horizontal">
+                        <Heading as="h1">The wild oasis</Heading>
+                        <div>
+                            <Heading as="h2">check in and out</Heading>
+                            <Button>check in</Button>
+                            <Button variations="secondary" size="small">check out</Button>
+                        </div>
+                    </Row>
+                    <Row>
+                        <Heading as="h3">Form</Heading>
+                        <form>
+                            <Input type="number" placeholder="Number of guests"/>
+                            <Input type="number" placeholder="Number of guests"/>
+                        </form>
+                    </Row>
+                </Row>
             </StyledApp>
         </>
     );
 }
 
 export default App;
-// E5 was finished.
+// E7 was finished.
