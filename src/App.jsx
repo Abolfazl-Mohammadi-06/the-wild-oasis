87f2552306a73fa74dcf1fd9c08d15533265411c
AbolfazlMohammadi <abolfazl.mohammadi.85.2006@gmail.com>
2026-09-01 12:55:46 +0330

    styling the base styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import styled, {css} from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle.js";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
+import Heading from "./ui/Heading.jsx";
+
+const test = css`
+    text-align: center;
+`;
+const StyledApp = styled.div`
+    background-color: orangered;
+    padding: 20px;
+    ${test}
+`;
+
 function App() {
     return (
-        <div>
-            Hello World !
-        </div>
+        <>
+            <GlobalStyle/>
+            <StyledApp>
+                <Heading as="h1">The wild oasis</Heading>
+                <Heading as="h2">check in and out</Heading>
+                <Button>check in</Button>
+                <Button>check out</Button>
+                <Heading as="h3">Form</Heading>
+                <Input type="number" placeholder="Number of guests"/>
+            </StyledApp>
+        </>
     );
 }
 
 export default App;
+// E5 was finished.
