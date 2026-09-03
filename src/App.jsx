586229aadca4e5d3dc4a176564fff4ad4d99d427
AbolfazlMohammadi <abolfazl.mohammadi.85.2006@gmail.com>
2026-09-03 21:46:40 +0330

    fetching cabin data

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,25 @@ import Login from "./pages/Login.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import GlobalStyle from "./styles/GlobalStyle.js";
 import AppLayout from "./ui/AppLayout.jsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            // staleTime: 60 * 1000,
+            staleTime: 0,
+        }
+    }
+})
+
 
 function App() {
     return (
-        <>
+
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false}/>
             <GlobalStyle/>
             <BrowserRouter>
                 <Routes>
@@ -30,10 +45,11 @@ function App() {
 
                 </Routes>
             </BrowserRouter>
-        </>
+        </QueryClientProvider>
+
     );
 }
 
 export default App;
-// S26 E9 was finished
+// S27 E5 was finished
 
